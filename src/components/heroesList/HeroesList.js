2e205f14d465c9f4cab6dbebf4ea07d6cff83572
zukolime <alena.perminova.1993@gmail.com>
2025-06-04 16:13:22 +0500

    update HeroesList

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -29,7 +29,9 @@ const HeroesList = () => {
   );
 
   const filteredHeroes = useSelector(filteredHeroesSelector);
-  const heroesLoadingStatus = useSelector((state) => state.heroesLoadingStatus);
+  const heroesLoadingStatus = useSelector(
+    (state) => state.heroes.heroesLoadingStatus
+  );
   const dispatch = useDispatch();
   const { request } = useHttp();
 
